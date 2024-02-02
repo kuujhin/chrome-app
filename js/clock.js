1cc024323443e702c7c485cd9f27f3afd40b361c
kuujhin <kdoubleu0711@gmail.com>
2024-02-02 13:47:33 +0900

    add date

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,13 @@
 const clock = document.querySelector("h2#clock");
+const day = document.querySelector("h2#date");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = date.toLocaleTimeString("en-US", { hour12: false });
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
+  day.innerText = date.toLocaleDateString("ko-KR");
+  // clock.innerText = date.toLocaleTimeString("en-US", { hour12: false });
 }
 
 getClock();
