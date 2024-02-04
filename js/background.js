9002c8a6f41e5e6ce9305b1b9c161f56bad7cfad
kuujhin <kdoubleu0711@gmail.com>
2024-02-05 00:41:40 +0900

    Adjust background brightness

--- js/background.js
+++ js/background.js
@@ -6,18 +6,16 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-// 배경에 사진넣기1
-document.body.style.backgroundImage = `url(img/${chosenImage})`;
-document.body.style.backgroundPosition = "top";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundAttachment = "fixed";
-
+// // 배경에 사진넣기1
+// document.body.style.backgroundImage = `url(img/${chosenImage})`;
+// document.body.style.backgroundPosition = "top";
+// document.body.style.backgroundRepeat = "no-repeat";
+// document.body.style.backgroundSize = "cover";
+// document.body.style.backgroundAttachment = "fixed";
 // 배경에 사진넣기2
 // style.css에 bgimg 설정
-// bgImage.id = "bgimg";
-
-// document.body.appendChild(bgImage);
+bgImage.id = "bgimg";
+document.body.appendChild(bgImage);
 
 // // body 앞에 추가
 //document.body.prepend(bgImage);
